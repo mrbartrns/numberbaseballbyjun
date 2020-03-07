f9eb9982679f43393ed556b0e4874d04ea41ed2b
Junhyuck Ko <mrbartrns@gmail.com>
2020-03-07 12:28:56 +0900

    testing파일

---
+++ testing.py
@@ -0,0 +1,10 @@
+class QuestionNumber:
+    """
+    이 클래스는 랜덤한 숫자를 배열로 만들어 저장하는 클래스입니다.
+    """
+    def __init__(self):
+        self.question_array = []
+
+    def __repr__(self):
+        return str(self.question_array)
+
