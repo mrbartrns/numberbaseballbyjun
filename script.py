f8bc2fe2716cd6331eeec9a06f3548a7acd463cd
Junhyuck Ko <mrbartrns@gmail.com>
2020-03-07 12:12:10 +0900

    룰 설명 추가

---
+++ script.py
@@ -0,0 +1,23 @@
+import random
+
+def welcome():
+    """
+    Welcome message at beginning of game with basic win conditions
+    :return: None
+    """
+    welcome_string = "LET'S NUMBER BASEBALL BEGINS!"
+    print("\n" + "#" * len(welcome_string))
+    print(f"{welcome_string}")
+    print("#" * len(welcome_string) + "\n")
+    print(" - 게임의 룰은 간단합니다; \
+4개의 무작위한 숫자가 주어질 것입니다.")
+    print(" - 숫자의 범위는 0에서 9까지이며, 숫자는 중복되지 않습니다.")
+    print(" - 숫자의 위치와 종류를 모두 맞추게되면 승리하게 됩니다!\n")
+    rule = "게임 규칙 설명:"
+    print(f"{rule}")
+    print("S (Strike): 숫자가 존재하고 그 숫자의 위치가 정확함")
+    print("B (Ball): 숫자가 존재하나 그 숫자의 위치가 틀림")
+    print("OUT (Out): 숫자가 배열에 존재하지 않음")
+
+
+welcome()
