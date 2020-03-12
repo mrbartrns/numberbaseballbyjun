c0c123228372798857a6838bbf3cdfa527c8e88d
Junhyuck Ko <mrbartrns@gmail.com>
2020-03-12 17:15:29 +0900

    'n번째 숫자를 입력하세요'로 나오도록 수정

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import random
 from function import *
 
 init_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+SCRIPT_VALUE_DIC = {'1': '첫', '2': '두', '3': '세', '4': '네'}
 
 
 def welcome():
@@ -125,19 +126,24 @@ def play_game():
     number_array = NumberIndex()
     while round_on:
         num_flag = True
+        script_key = 1
         while num_flag:
             try:
-                num = int(input('숫자를 입력하세요: '))
+                script_value = SCRIPT_VALUE_DIC[str(script_key)]
+                script_value_input_string = f'{script_value}번째 값을 입력하세요:'
+                print(script_value_input_string, end=' ')
+                num = int(input())
                 num_checking = num_check(num, answer_array.answer_array)
                 if num_checking == -1:
                     print('첫째 자리에는 0이 입력될 수 없습니다.')
                 elif num_checking == 0:
+                    script_key += 1
                     pass
                 elif num_checking == 1:
                     print('중복되는 숫자를 입력할 수 없습니다.')
                 elif num_checking == 2:
                     print('0보다 크거나 같고 10보다 작은 수를 입력하세요.')
-                if len(answer_array.answer_array) == 4:
+                if len(answer_array) == 4:
                     num_flag = False
             except ValueError:
                 print('숫자만 입력할 수 있습니다.')
