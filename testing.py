fc0927f2eeef75822aeafccf9abac249960ad2d9
Junhyuck Ko <mrbartrns@gmail.com>
2020-03-12 17:15:45 +0900

    새로운 실험 코드

--- testing.py
+++ testing.py
@@ -1,44 +1,53 @@
 from script import *
+from function import *
+
+s_val = 0
+SCRIPT_VALUE_DIC = {'1': '첫', '2': '두', '3': '세', '4': '네'}
 
 
-def num_check(num, answer_array_f):
-    if 0 <= num < 10:
-        answer_array_f.append(num)
-        # 첫째 짜리에 0을 입력받을 경우, 다시 끄집어냄
-        if answer_array_f[0] == 0:
-            answer_array_f.pop()
-            return -1
-        # 위의 조건을 모두 만족시, array의 길이와 중복되는 숫자 검사
-        else:
-            # count_array는 숫자의 종류를 저장하는 배열로 rule_array와 동일한 역할을 하지만 길이가 다름
-            count_array = []
-            for i in range(10):
-                count_array.append(answer_array_f.count(i))
-            # 중복시 숫자를 끄집어냄
-            for i in count_array:
-                if i == 2:
-                    answer_array_f.pop()
-                    return 1
-            return 0
-    else:
-        return 2
+# Todo: 'n번째 숫자를 입력하세요.'로 변경하기
+def play_game_test():
+    welcome()
+    round_on = True
+    test_array = QuestionNumber()
+    answer_array = AnswerNumber()
+    rule_array = Rule(test_array.test_array)
+    number_array = NumberIndex()
+    while round_on:
+        num_flag = True
+        script_key = 1
+        while num_flag:
+            try:
+                script_value = SCRIPT_VALUE_DIC[str(script_key)]
+                script_value_input_string = f'{script_value}번째 값을 입력하세요:'
+                print(script_value_input_string, end=' ')
+                num = int(input())
+                num_checking = num_check(num, answer_array.answer_array)
+                if num_checking == -1:
+                    print('첫째 자리에는 0이 입력될 수 없습니다.')
+                elif num_checking == 0:
+                    script_key += 1
+                    pass
+                elif num_checking == 1:
+                    print('중복되는 숫자를 입력할 수 없습니다.')
+                elif num_checking == 2:
+                    print('0보다 크거나 같고 10보다 작은 수를 입력하세요.')
+                if len(answer_array) == 4:
+                    num_flag = False
+            except ValueError:
+                print('숫자만 입력할 수 있습니다.')
+        rule_array.rule_append(answer_array.answer_array)
+        number_array.num_append(rule_array.rule_array)
+        rule_check_test = rule_check(test_array.test_array, answer_array.answer_array, number_array.number_array)
+        if rule_check_test == -1:
+            print('OUT')
+        elif rule_check_test[0] == 0:
+            print('%dB' % rule_check_test[1])
+        elif rule_check_test[1] == 0:
+            print('%dS' % rule_check_test[0])
+        elif rule_check_test[0] != 0 and rule_check_test[1] != 0:
+            print('%dS %dB' % (rule_check_test[0], rule_check_test[1]))
+        break
 
 
-answer = []
-num_flag = True
-while num_flag:
-    try:
-        num = int(input('숫자를 입력하세요: '))
-        num_checking = num_check(num, answer)
-        if num_checking == -1:
-            print('첫째 자리에는 0이 입력될 수 없습니다.')
-        elif num_checking == 0:
-            pass
-        elif num_checking == 1:
-            print('중복되는 숫자를 입력할 수 없습니다.')
-        elif num_checking == 2:
-            print('0보다 크거나 같고 10보다 작은 수를 입력하세요.')
-        if len(answer) == 4:
-            num_flag = False
-    except ValueError:
-        print('숫자만 입력할 수 있습니다.')
+play_game_test()
